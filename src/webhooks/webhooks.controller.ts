c63e975b66141ed27b546b4918946bf71e7e8c5b
Ahmed Aslam <ahmed.kamal@devsinc.com>
2026-02-06 15:29:34 +0500

    Added PR events check to optimize the webhook processing

--- src/webhooks/webhooks.controller.ts
+++ src/webhooks/webhooks.controller.ts
@@ -1,15 +1,24 @@
-import { Controller, Post } from '@nestjs/common';
+import { Controller, Headers, Post } from '@nestjs/common';
 import { Body } from '@nestjs/common';
 import { WebhooksService } from './webhooks.service';
 
 @Controller('webhooks')
 export class WebhooksController {
-    constructor(private readonly webhooksService: WebhooksService) {}
+    constructor(private readonly webhooksService: WebhooksService) { }
 
 
     @Post()
-    createWebhook(@Body() webhook: any) {
-        console.log(webhook);
-        return this.webhooksService.addWebhook(webhook);
+    createWebhook(@Body() webhook: any,
+        @Headers('x-github-event') event: string,
+    ) {
+        if (event === 'pull_request') {
+            if(webhook.action === 'opened') {
+                return this.webhooksService.addWebhook(webhook);
+            } else {
+                return {
+                    message: 'Pull request not opened',
+                };
+            }
+        }
     }
 }
